9ec8db8018cde381a4bdbbfd5a72a1ff3c4ee37a
nmanikiran <nmanikiran15@gmail.com>
2020-04-19 20:41:53 +0530

    remove un used plugin

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "My App",
-      filename: "assets/admin.html"
-    })
-  ]
+  transpileDependencies: ["vuetify"]
 };
